10da88d8bf1d5f342767c825f87b12500e5b03f7
MopyKing <roye16e@gmail.com>
2022-04-14 12:59:36 +0300

    fixed command for path

--- shido.java
+++ shido.java
@@ -1,7 +1,7 @@
 public class shido{
   public static void main(String[] args) {
     for (int i=1;i<=10;i++){
-      System.out.println("Shido V2.."+i);
+      System.out.println("Shido V3.."+i);
     }
   }
 }
