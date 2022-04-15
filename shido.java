f47f4ae6968b8ba33586cd891e2b790525ebdb98
MopyKing <roye16e@gmail.com>
2022-04-15 16:30:46 +0300

    6th check for cd command after granting permissions with chmod

--- shido.java
+++ shido.java
@@ -1,7 +1,7 @@
 public class shido{
   public static void main(String[] args) {
     for (int i=1;i<=10;i++){
-      System.out.println("Shido V8.."+i);
+      System.out.println("Shido V9.."+i);
     }
   }
 }
